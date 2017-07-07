e23c1963a1b13a1ae7121521584d03d6cd22ad77
VanStratum <arthur.van.stratum@gmail.com>
2017-07-07 16:23:02 +0200

    added license information

--- survey.py
+++ survey.py
@@ -1,23 +1,10 @@
 # -*- coding: utf-8 -*-
 #
-#    This file belongs to the ICTV project, written by Nicolas Detienne,
-#    Francois Michel, Maxime Piraux, Pierre Reinbold and Ludovic Taffin
-#    at Universite Catholique de Louvain.
-#
-#    Copyright (C) 2017  Universite Catholique de Louvain (UCL, Belgium)
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#    This file is  written by Arnaud Gellens,Arthur van Stratum,
+#    Céline Deknop, Charles-Henry Bertrand Van Ouytsel,
+#    Margerie Huet and Simon Gustin during the OpenWeek 2017 at
+#    Universite Catholique de Louvain.
+#    This software is licensed under the MIT License
 
 
 import web
